ffed9d92096b67c2ea49350be17660fc8ad8977d
Arteems <artm_frolov00@mail.ru>
2024-08-02 20:12:41 +0200

    add more

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from pydantic import EmailStr, BaseModel
+from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as users_router
 import uvicorn
 
 
-app = FastAPI()
-app.include_router(
-    items_router,
-)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_async(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(items_router)
 app.include_router((users_router))
-
-
-
 
 
 @app.get("/")
@@ -26,9 +30,6 @@ def hello_index():
 def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"Hello {name}"}
-
-
-
 
 
 @app.post("/calc/add/")
